1b6e18a52bf2f2eea7adcb54d5a217a26fa08abd
Alessia Moison <adm2217@columbia.edu>
2024-10-15 16:51:41 +0200

    absolute and relative layout-conclusion of layout section

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ export default function App() {
     // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
       {/* Box text is passed to the children prop and backgroundColor the style prop, which will be merged with styles.box in Box.js */}
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", top: 75, left: 75 }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#ab9156", position: "absolute", top: 100, left: 100 }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
